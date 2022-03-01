c644eee36eddcc17d3fe9b2681b1446f8b58b1e0
Marcos Nunes <marcosnunesmbs@gmail.com>
2022-03-01 14:39:40 -0300

    controlers and guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { SigninDto } from './dto/signin.dto';
+import { SignupDto } from './dto/signup.dto';
+import { User } from './models/users.model';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {
+
+    }
+
+    @Post('signup')
+    @HttpCode(HttpStatus.CREATED)
+    public async signup(@Body() signupDto: SignupDto): Promise<User> {
+        return this.usersService.siginUp(signupDto);
+    }
+
+    @Post('signin')
+    @HttpCode(HttpStatus.OK)
+    public async signin(@Body() signinDto: SigninDto): Promise<{
+        name: string, jwtToken: string, email: string
+    }> {
+        return this.usersService.siginIn(signinDto);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(AuthGuard('jwt'))
+    public async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+}
